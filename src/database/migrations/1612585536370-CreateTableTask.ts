98e3f28b5ebd798ec8807978013f21f3ca87b3c7
antonylrds <42950733+antonylrds@users.noreply.github.com>
2021-02-06 11:43:46 -0300

    Miagration de criação de table tasks

---
+++ src/database/migrations/1612585536370-CreateTableTask.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTableTask1612585536370
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('task');
+  }
+}
